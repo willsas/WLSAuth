7e0b70f923bde4bae2af628e3f936e46d2533c63
Willa <willasaskara@gmail.com>
2021-06-26 02:06:38 +0700

    init cocoapod

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WLSAuth' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WLSAuth
+
+  target 'WLSAuthTests' do
+    # Pods for testing
+  end
+
+end
---
+++ WLSAuth/WLSAuth.h
@@ -0,0 +1,18 @@
+//
+//  WLSAuth.h
+//  WLSAuth
+//
+//  Created by Willa on 26/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WLSAuth.
+FOUNDATION_EXPORT double WLSAuthVersionNumber;
+
+//! Project version string for WLSAuth.
+FOUNDATION_EXPORT const unsigned char WLSAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WLSAuth/PublicHeader.h>
+
+
